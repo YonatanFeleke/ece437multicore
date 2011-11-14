9e86ee43950badb1a4f5d3d7004eeb99fa51297e
bcrone146 <bcrone146@1069743e-ead5-a84e-a039-e34a2020b4cb>
2011-11-14 23:28:01 +0000

    New Arbitor File

--- trunk/Yproject4/source/Arbitor.vhd
+++ trunk/Yproject4/source/Arbitor.vhd
@@ -26,8 +26,6 @@ ENTITY Arbitor IS
       aiMemRead1 : IN     std_logic;
       aiMemData1 : OUT    std_logic_vector (31 DOWNTO 0);
       iMemRead1  : OUT    std_logic;
-      ramRen     : OUT    std_logic;
-      ramWen     : OUT    std_logic;
       aiMemRead0 : IN     std_logic;
       aiMemAddr0 : IN     std_logic_vector (31 DOWNTO 0);
       aMemRead   : IN     std_logic;
@@ -40,8 +38,10 @@ ENTITY Arbitor IS
       iMemRead0  : OUT    std_logic;
       aiMemData0 : OUT    std_logic_vector (31 DOWNTO 0);
       aMemWrData : IN     std_logic_vector (31 DOWNTO 0);
+      ramAddr    : OUT    std_logic_vector (15 DOWNTO 0);
       ramData    : OUT    std_logic_vector (31 DOWNTO 0);
-      ramAddr    : OUT    std_logic_vector (15 DOWNTO 0)
+      ramRen     : OUT    std_logic;
+      ramWen     : OUT    std_logic     
    );
 
 -- Declarations
@@ -55,49 +55,121 @@ constant MEMBUSY        : std_logic_vector              := "01";
 constant MEMACCESS      : std_logic_vector              := "10";
 constant MEMERROR       : std_logic_vector              := "11";
 
-  signal serving :std_logic_vector(1 downto 0); --01 data    10 icache0    11 icache1
-BEGIN
-  serving <= "01" when (ramState = MEMFREE and (aMemRead = '1' or aMemWrite ='1')) else
-  						   "10" when (ramstate = MEMFREE and aiMemRead0 ='1') else
-						   "11" when (ramstate = MEMFREE and aiMemRead1 ='1') else
-					     "00";
-  ramAddr <= aMemAddr(15 downto 0) when serving = "01" else
-  					 aiMemAddr0(15 downto 0) when serving ="10" else
-  					 aiMemAddr1(15 downto 0) when serving ="11" else
-  					 (others => '1');
-	ramData <= aMemWrData when serving = "01" else x"00000000";
-	ramWen <= aMemWrite when serving ="01" else '0';
-	arbWait0 <= '1' when serving = "10" else '0';
-	arbWait1 <= '1' when serving = "11" else '0';
-	iMemRead0 <= '1' when serving = "10" else '0';
-	iMemRead1 <= '1' when serving = "11" else '0';
-  busy  <= '0' when serving = "01" else '1';	
-	
-	  readENState: process( clk, nReset,ramState)
-    begin 
-    	if nReset= '0' then 
-    	  ramRen <= '0';
-       aiMemData0 <= (others => '0');
-       aiMemData1 <= (others => '0');
-       aMemRdData <= (others => '0');
-	    elsif (rising_edge(clk))   then  -- probably won't use aMemWait
-       aiMemData0 <= (others => '0');
-       aiMemData1 <= (others => '0');
-       aMemRdData <= (others => '0');
-	      if (ramState = MEMACCESS) then 
-	        ramRen <= '0';
-	      end if;
-	      if serving = "01" then -- updates only on MemFree hence on MemFree and rising clk
-	        ramRen <= aMemRead;
- 	        aMemRdData <= ramQ;      
-	      elsif serving = "10" then
-	        ramRen <= aiMemRead0;
-	        aiMemData0 <= ramQ;
-	      elsif serving = "10" then
-	        ramRen <= aiMemRead1;
-	        aiMemData1 <= ramQ;
-	      end if;
-    		end if;
-  end process readENState;
+
+
+
+	type state_type is(idle, icache0, icache1, data);
+	signal state, nextstate						:	state_type;
+  signal servicing :std_logic_vector(1 downto 0); --01 data    10 icache0    11 icache1
+	signal prevCache, nextprevCache : std_logic;
+	signal aiMemAddr1_int : std_logic_vector(31 downto 0);
+component Add32 is				
+    port( A32, B32 : in std_logic_vector(31 downto 0);
+          Add32Out : out std_logic_vector(31 downto 0));
+--          NEGATIVE, OVERFLOW, ZERO: out std_logic );
+  end component;
+
+begin
+
+cctrl_state: process(CLK, nReset)
+begin
+	if nReset = '0' then
+		-- set state to idle
+		state <= idle;
+		prevCache <= '1';
+	elsif rising_edge(CLK) then
+		-- set state to next state
+		state <= nextstate;
+		prevCache <= nextprevCache;
+	end if;
+end process cctrl_state;
+
+memControl_nextstate: process(state, aMemRead, aMemWrite, aiMemRead0,aiMemRead1, prevCache, ramState) begin
+nextstate<=state;  
+servicing <= "00";
+nextprevCache <= prevCache;
+case state is 
+	when idle => 
+		servicing <= "00";
+		if(aMemRead = '1' or aMemWrite = '1') then--CoherenceController request
+			servicing <= "01";
+			nextstate <= data;
+		elsif(aiMemRead0 = '1' or aiMemRead1 = '1') then-- no data request. service icaches
+			if(aiMemRead0 = '1') then--cache0 request
+				if(aiMemRead1 = '1') then--cache1 and cache0 request
+					if(prevCache = '0') then--priority to cache1
+						servicing <= "11";
+						nextstate <= icache1;
+					end if;
+				else--request granted to cache0
+					servicing <= "10";
+					nextstate <= icache0;
+				end if;
+			else--cache1 only request
+				servicing <= "11";
+				nextstate <= icache1;
+			end if;
+		end if;
+	when icache0 =>
+		servicing <= "10";
+		nextstate <= icache0;		
+		if (ramState = MEMACCESS) then
+			nextprevCache <= '0';
+			nextstate <= idle;
+		end if;
+	when icache1 =>
+		servicing <= "11";
+		nextstate <= icache1;
+		if (ramState = MEMACCESS) then
+			nextprevCache <= '1';
+			nextstate <= idle;
+		end if;
+	when data =>--will remain in data until MEMFREE. will go to idle if there are no requests from either icahce. Will go straight to appropriate icache state if requests exist
+		servicing <= "01";
+		if (ramstate = MEMFREE) then
+			servicing <= "00";
+			if(aiMemRead0 = '1') then--cache0 request
+				if(aiMemRead1 = '1') then--cache1 and cache0 request
+					if(prevCache = '0') then--priority to cache1
+						nextstate <= icache1;
+					else
+						nextstate <= icache0; -- priority to cache0
+					end if;
+				else--cache0 only
+					nextstate <= icache0;
+				end if;
+			elsif(aiMemRead1 = '1') then--cache1 only
+				nextstate <= icache1;
+			else--return to idle
+				nextstate <= idle;
+			end if;
+		end if;
+	end case;
+end process memControl_nextstate;
+
+--SERVICING:
+--00 = No control
+--01 = dCache(coherence) control
+--10 = icache0 control
+--11 = icache1 control
+Add200 : Add32 port map(a32 => aiMemAddr1,b32 => x"00000200", add32out => aiMemAddr1_int);
+      aiMemData1 <= ramQ when servicing = "11" else x"00000000";
+      iMemRead1 <= '1' when servicing = "11" else '0';
+      aMemRdData <= ramQ when servicing = "01" else x"00000000";
+      arbWait1 <= '1' when servicing = "11" else '0';
+      arbWait0 <= '1' when servicing = "10" else '0';
+      busy <= '0' when servicing = "01" else '1';
+      iMemRead0 <= '1' when servicing = "10" else '0';
+      aiMemData0 <= ramQ when servicing = "10" else x"00000000";
+      ramAddr <= aMemAddr(15 downto 0) when servicing = "01" else
+								 aiMemAddr0(15 downto 0) when servicing = "10" else
+								 aiMemAddr1_int(15 downto 0) when servicing = "11" else
+								 (others => '1');					
+      ramData <= aMemWrData;
+      ramRen <= aiMemRead0 when servicing = "10" else
+								aiMemRead1 when servicing = "11" else
+								aMemRead when servicing = "01" else
+								'0';
+      ramWen <= aMemWrite when servicing = "01" else '0';
 END ARCHITECTURE arch_name;
 
