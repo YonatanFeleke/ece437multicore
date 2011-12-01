974d456c9b4c9bce90c574c1c1ec1bba9aac08de
yoniaberra@gmail.com <yoniaberra@gmail.com@1069743e-ead5-a84e-a039-e34a2020b4cb>
2011-12-01 04:13:07 +0000

    copied tagarray and updated SLT

--- trunk/Yproject4/source/SLT.vhd
+++ trunk/Yproject4/source/SLT.vhd
@@ -17,19 +17,20 @@ architecture SLT_arch of SLT is
 	signal diff: std_logic;
   begin    
 	unsignedCompare: process(A,B,Opcode,Fx)
+	variable diff : std_logic;
   begin
 		SLTunsigned <= '0';
-		diff <= '0';
+		diff := '0';
 		for i in 31 downto 0 loop
-			if((A(i) xor B(i)) = '1' and diff = '0') then
-				SLTunsigned <= not A(i);
-				diff <= '1';
+			if(((A(i) xor B(i)) = '1') and diff = '0') then
+				SLTunsigned <= not(A(i));
+				diff := '1';
 			end if;
 		end loop;
 	end process unsignedCompare;
-
+  --SLTunsigned <= '1' when (A < B) else '0';
 	SLTsigned <= Negative when Overflow = '0' else A(31);--Negative flag unless there is overflow. else sign bit of A
-	SLTvalue <= SLTunsigned when (Opcode = "001011") or (Opcode = "000000" and Fx = "1011011") else
+	SLTvalue <= SLTunsigned when (Opcode = "001011") or (Opcode = "000000" and Fx = "101011") else
 							SLTsigned when (Opcode = "001010") or (Opcode = "000000" and Fx = "101010") else
 							'0';
 		
