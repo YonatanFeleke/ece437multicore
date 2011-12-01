44ab9a890ccbd4ab3a74c21640bf1eb11d6fc8a5
yoniaberra@gmail.com <yoniaberra@gmail.com@1069743e-ead5-a84e-a039-e34a2020b4cb>
2011-12-01 01:40:54 +0000

    Threw in SLT in LocalCPU brian work further

--- Yproject4/source/DataRtn.vhd
+++ Yproject4/source/DataRtn.vhd
@@ -7,7 +7,7 @@ USE ieee.std_logic_arith.all;
 
 ENTITY DataRtn IS
    PORT( 
-      SLT_EN,LUI,NEG  : IN     std_logic;      
+      SLT_EN,LUI,SLTvalue  : IN     std_logic;      
       JAL										  : IN     std_logic; 
       Imm16					  				: IN     std_logic_vector (15 DOWNTO 0);
       PC_4,EX_AluOut  			: IN 		 std_logic_vector(31 downto 0);
@@ -19,8 +19,7 @@ ARCHITECTURE Behav_DataRtn OF DataRtn IS
 	signal NegMuxOut,SLT_ENMuxOut,LUIMuxOut,Im32 : std_logic_vector( 31 downto 0);
 BEGIN
 	Im32 <= Imm16 & x"0000";
-	NegMuxOut <= x"00000001" when Neg = '1' else x"00000000";
-	SLT_ENMuxOut <=  NegMuxOut when SLT_EN = '1' else EX_AluOut;
+	SLT_ENMuxOut <=  x"0000000"&"000"&SLTvalue when SLT_EN = '1' else EX_AluOut;
 	LUIMuxOut <= Im32 when LUI = '1' else SLT_ENMuxOut;
 	EX_Out <= PC_4 when JAL ='1' else LUIMuxOut;
 END ARCHITECTURE Behav_DataRtn;
