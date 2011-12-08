a5ed7cabf4bcf5b0d8c7cdda9a757876af958840
yoniaberra@gmail.com <yoniaberra@gmail.com@1069743e-ead5-a84e-a039-e34a2020b4cb>
2011-12-08 02:26:52 +0000

    only llscs breaking

--- Yproject4/source/J_JAL.vhd
+++ Yproject4/source/J_JAL.vhd
@@ -3,7 +3,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity J_JAL is				
-    port( IF_Instr             					: in std_logic_vector(31 downto 0);          
+    port( IF_Instr             					: in std_logic_vector(31 downto 0);
+          ID_PcSrc                			: in std_logic;          
           IF_JAddr26              			: out std_logic_vector(25 downto 0);
           IF_JMP                   			: out std_logic
          );
@@ -16,5 +17,5 @@ architecture JJAL_behav of J_JAL is
 											--  OpCode_Int(5 downto 1) = "00001"
       JTYPE <= '1' when  (IF_Instr(31 downto 27) = "00001") else '0';   	      	
 			IF_JAddr26 <= IF_Instr(25 downto 0) when JTYPE ='1' else  "00"& x"000000";-- JAL 
-			IF_JMP <= '1' when JTYPE='1' else '0';
+			IF_JMP <= '1' when JTYPE='1' and ID_PCSrc='0' else '0';
 end JJAL_behav;
