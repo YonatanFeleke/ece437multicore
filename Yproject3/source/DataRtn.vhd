0d19587df59fa2cdf9624f3b57539f5e719fe089
yoniaberra@gmail.com <yoniaberra@gmail.com@1069743e-ead5-a84e-a039-e34a2020b4cb>
2011-12-09 01:34:16 +0000

    Done with Yproject3 passing all local tests

--- Yproject3/source/DataRtn.vhd
+++ Yproject3/source/DataRtn.vhd
@@ -2,12 +2,10 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_arith.all;
---LIBRARY altera_mf;
---USE altera_mf.all;
 
 ENTITY DataRtn IS
    PORT( 
-      SLT_EN,LUI,NEG  : IN     std_logic;      
+      SLT_EN,LUI,SLTvalue  : IN     std_logic;      
       JAL										  : IN     std_logic; 
       Imm16					  				: IN     std_logic_vector (15 DOWNTO 0);
       PC_4,EX_AluOut  			: IN 		 std_logic_vector(31 downto 0);
@@ -19,8 +17,7 @@ ARCHITECTURE Behav_DataRtn OF DataRtn IS
 	signal NegMuxOut,SLT_ENMuxOut,LUIMuxOut,Im32 : std_logic_vector( 31 downto 0);
 BEGIN
 	Im32 <= Imm16 & x"0000";
-	NegMuxOut <= x"00000001" when Neg = '1' else x"00000000";
-	SLT_ENMuxOut <=  NegMuxOut when SLT_EN = '1' else EX_AluOut;
+	SLT_ENMuxOut <=  x"0000000"&"000"&SLTvalue when SLT_EN = '1' else EX_AluOut;
 	LUIMuxOut <= Im32 when LUI = '1' else SLT_ENMuxOut;
 	EX_Out <= PC_4 when JAL ='1' else LUIMuxOut;
 END ARCHITECTURE Behav_DataRtn;
